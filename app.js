e069833726f1b70d27d5164b5a5972e69f36b2ac
Dhruvesh1617 <shettydhruvesh16@gmail.com>
2021-01-03 12:26:05 +0530

    input textarea and button

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var btntranslate=document.querySelector("#btntranslate");
-btntranslate.addEventListener("click",function Onclicking(){
-    alert("clicked");
-    
-
-})
+var btntranslate=document.querySelector("#Click");
+var txtinput=document.querySelector("#txt-input");
+console.log(txtinput);
+function translate()
+{
+    console.log("clicked");
+    console.log("input",txtinput.value);
+}
+btntranslate.addEventListener("click",translate);
