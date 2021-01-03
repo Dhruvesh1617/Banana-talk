4cbcee963571d71cda08a03e8cf222f1f8441a39
Dhruvesh1617 <shettydhruvesh16@gmail.com>
2021-01-03 13:32:39 +0530

    I/O processing

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var btntranslate=document.querySelector("#Click");
 var txtinput=document.querySelector("#txt-input");
-console.log(txtinput);
+var txtoutput=document.querySelector("#txt-output");
+
 function translate()
 {
-    console.log("clicked");
-    console.log("input",txtinput.value);
+
+    txtoutput.innerText="asjjfkkfoskfkflsdsmv  " +txtinput.value;
 }
 btntranslate.addEventListener("click",translate);
+
+
+/** ways in which we can define query selector
+ * for html tag= eg(textarea=document.querySelector("textarea");)
+ *for html id= eg(textarea=document.querySelector("#txt-input");)
+ for html class= eg(textarea=document.querySelector(".textarea");)
+eg(textarea=document.querySelector(input[name="textarea"]);)
+ */
